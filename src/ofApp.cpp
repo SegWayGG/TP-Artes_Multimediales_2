67d90cc6747368becf9e65fbc24e408da65da564
SegWayGG <fransanti33@gmail.com>
2023-06-01 22:43:01 -0300

    Agregue un circulo que reacciona según la posición de los analógicos.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -10,38 +10,45 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-void ofApp::update() {
-
-	for (int i = 0; i < joystick.getButtonNum(); i++)
+void ofApp::update()
+{
+	if (joystick.isPushing(0))
 	{
-		if (joystick.isPressed(i))
-		{
-			ofLog() << "pressed : " << i;
-		}
-
-		if (joystick.isPushing(i))
-		{
-			ofLog() << "pushing : " << i;
-		}
-
-		if (joystick.isRelease(i))
-		{
-			ofLog() << "released : " << i;
-		}
+		ofSetColor(255, 0, 0);
 	}
+	else
+	{
+		ofSetColor(255);
+	}
+
+	float joyX = joystick.getAxis(0); //Devuelve la posición en X del analógico izquierdo
+	float joyY = joystick.getAxis(1); //Devuelve la posición en Y del analógico izquierdo	
+	//Los analógicos devuelven un valor entre -1 y 1.
+
+	x = ofMap(joyX, -1, 1, ancho/2, ofGetWidth()-ancho/2);
+	y = ofMap(joyY, -1, 1, alto/2, ofGetHeight()-alto/2);
+
+	float _ancho = joystick.getAxis(2); //Devuelve la posición en X del analógico derecho
+	float _alto = joystick.getAxis(3); //Devuelve la posición en Y del analógico derecho
+
+	ancho = ofMap(_ancho, -1, 1, 1, 200);
+	alto = ofMap(_alto, -1, 1, 200, 1);
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
+	ofDrawEllipse(x, y, ancho, alto);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key)
+{
 
 }
